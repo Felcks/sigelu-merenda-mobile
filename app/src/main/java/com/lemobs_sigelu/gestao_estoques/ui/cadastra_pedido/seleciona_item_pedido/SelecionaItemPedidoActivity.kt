e2dfa95bf7cb114515f11d956e7ec72d3e694d1c
Matheus Felipe <matheusfelipecorreaalves@gmail.com>
2019-06-19 20:26:42 -0300

    Inicio pedido activity

--- app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/seleciona_item_pedido/SelecionaItemPedidoActivity.kt
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/seleciona_item_pedido/SelecionaItemPedidoActivity.kt
@@ -1,16 +1,27 @@
 package com.lemobs_sigelu.gestao_estoques.ui.cadastra_pedido.seleciona_item_pedido
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.lemobs_sigelu.gestao_estoques.R
+import dagger.android.AndroidInjection
+import javax.inject.Inject
 
 /**
  * Created by felcks on Jun, 2019
  */
 class SelecionaItemPedidoActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @Inject
+    lateinit var viewModelFactory: SelecionaItemPedidoViewModelFactory
+    var viewModel: SelecionaItemPedidoViewModel? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleciona_material_pedido)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(SelecionaItemPedidoViewModel::class.java)
     }
 
 
