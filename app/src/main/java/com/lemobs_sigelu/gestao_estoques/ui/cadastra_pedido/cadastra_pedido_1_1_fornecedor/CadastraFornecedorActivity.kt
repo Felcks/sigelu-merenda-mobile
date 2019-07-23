90bf6e80b11dfbb26e3ccf35249ae7f48c909704
Felcks <matheusfelipecorreaalves@gmail.com>
2019-07-23 11:23:42 -0300

    Selecione um fornecedor pronto

--- app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/cadastra_pedido_1_1_fornecedor/CadastraFornecedorActivity.kt
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/cadastra_pedido_1_1_fornecedor/CadastraFornecedorActivity.kt
@@ -1,9 +1,17 @@
 package com.lemobs_sigelu.gestao_estoques.ui.cadastra_pedido.cadastra_pedido_1_1_fornecedor
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
 import com.lemobs_sigelu.gestao_estoques.R
+import com.lemobs_sigelu.gestao_estoques.common.domain.model.ContratoEstoque
+import com.lemobs_sigelu.gestao_estoques.common.domain.model.Fornecedor
+import com.lemobs_sigelu.gestao_estoques.common.domain.model.Local
+import com.lemobs_sigelu.gestao_estoques.common.viewmodel.Response
+import com.lemobs_sigelu.gestao_estoques.common.viewmodel.Status
 import com.lemobs_sigelu.gestao_estoques.ui.cadastra_pedido.CadastraPedidoViewModelFactory
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_cadastra_pedido_fornecedor.*
@@ -22,8 +30,13 @@ class CadastraFornecedorActivity: AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(CadastraFornecedorViewModel::class.java)
 
+        viewModel!!.responseEmpresas.observe(this, Observer<Response> { response -> processResponse(response) })
+        viewModel!!.responseContratos.observe(this, Observer<Response> { response -> processResponse(response) })
+
+        viewModel!!.carregaListaFornecedor()
+
         ll_layout_anterior.setOnClickListener {
-            onBackPressed()
+            clicouNoAnterior()
         }
 
         ll_layout_proximo.setOnClickListener {
@@ -37,5 +50,43 @@ class CadastraFornecedorActivity: AppCompatActivity() {
 
     private fun clicouNoAnterior(){
         this.onBackPressed()
+    }
+
+    fun processResponse(response: Response?) {
+        when (response?.status) {
+            Status.LOADING -> {}
+            Status.SUCCESS -> {
+
+                if(response.data is List<*>){
+
+                    if(response.data[0] is Fornecedor)
+                        renderDataFornecedor(response.data)
+
+                    if(response.data[0] is ContratoEstoque)
+                        renderDataContrato(response.data)
+                }
+
+            }
+            Status.ERROR -> {}
+        }
+    }
+
+    private fun renderDataFornecedor(result: Any?) {
+
+        val list = result as List<Fornecedor>
+
+        val listaTextoOrigem = list.map { it.nome }
+        var adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,  listaTextoOrigem)
+        spinner_fornecedor.adapter = adapter
+
+        //spinner_fornecedor.onItemSelectedListener = viewModel!!.selecionadorOrigem
+    }
+
+    private fun renderDataContrato(result: Any?) {
+
+//        if(result is List<*>){
+//            viewModel!!.listaContrato.addAll((result as List<ContratoEstoque>))
+//        }
+//        iniciarSpinnerContratos()
     }
 }
--- app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/cadastra_pedido_1_1_fornecedor/CadastraFornecedorViewModel.kt
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/cadastra_pedido_1_1_fornecedor/CadastraFornecedorViewModel.kt
@@ -1,8 +1,53 @@
 package com.lemobs_sigelu.gestao_estoques.ui.cadastra_pedido.cadastra_pedido_1_1_fornecedor
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.lemobs_sigelu.gestao_estoques.common.domain.interactors.CadastraPedidoController
+import com.lemobs_sigelu.gestao_estoques.common.viewmodel.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class CadastraFornecedorViewModel (val controller: CadastraPedidoController): ViewModel() {
+
+
+    private val disposables = CompositeDisposable()
+
+    val responseEmpresas = MutableLiveData<Response>()
+    val responseContratos = MutableLiveData<Response>()
+
+    fun carregaListaFornecedor(){
+
+        disposables.add(controller.carregaListaFornecedor()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { responseEmpresas.setValue(Response.loading()) }
+            .subscribe(
+                { result ->
+                    responseEmpresas.value = Response.success(result)
+                },
+                { throwable ->
+                    responseEmpresas.value = Response.error(throwable)
+                }
+            )
+        )
+    }
+
+    fun carregaListaContrato() {
+
+        disposables.add(controller.carregaListaContrato()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { responseContratos.setValue(Response.loading()) }
+            .subscribe(
+                { result ->
+                    responseContratos.value = Response.success(result)
+                },
+                { throwable ->
+                    responseContratos.value = Response.error(throwable)
+                }
+            )
+        )
+    }
 
 }
