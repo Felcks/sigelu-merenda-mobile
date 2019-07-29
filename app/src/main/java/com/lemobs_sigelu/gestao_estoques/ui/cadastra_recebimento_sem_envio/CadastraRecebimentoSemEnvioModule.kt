3b8729b5e537c98621156cc9c7e3e4861dfb1830
Felcks <matheusfelipecorreaalves@gmail.com>
2019-07-29 17:17:08 -0300

    Cadastra recebimento sem envio - 3

--- app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_recebimento_sem_envio/CadastraRecebimentoSemEnvioModule.kt
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_recebimento_sem_envio/CadastraRecebimentoSemEnvioModule.kt
@@ -1,6 +1,7 @@
 package com.lemobs_sigelu.gestao_estoques.ui.cadastra_recebimento_sem_envio
 
 import com.lemobs_sigelu.gestao_estoques.common.domain.interactors.CadastraRecebimentoSemEnvioController
+import com.lemobs_sigelu.gestao_estoques.common.domain.repository.ItemPedidoRepository
 import dagger.Module
 import dagger.Provides
 
@@ -10,5 +11,10 @@ class CadastraRecebimentoSemEnvioModule
     @Provides
     fun provideViewModelFactory(controller: CadastraRecebimentoSemEnvioController): CadastraRecebimentoSemEnvioViewModelFactory{
         return CadastraRecebimentoSemEnvioViewModelFactory(controller)
+    }
+
+    @Provides
+    fun provideItemPedidoRepo(): ItemPedidoRepository{
+        return ItemPedidoRepository()
     }
 }
