b7ebbb42b4097ed82bf0dc3333ab56c9a4eb2ef9
Felcks <matheusfelipecorreaalves@gmail.com>
2019-08-01 12:25:16 -0300

    Visualiza estoque - 5 adapter pronto

--- app/src/main/java/com/lemobs_sigelu/gestao_estoques/common/domain/model/NucleoQuantidadeDeItemEstoque.kt
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/common/domain/model/NucleoQuantidadeDeItemEstoque.kt
@@ -3,4 +3,7 @@ package com.lemobs_sigelu.gestao_estoques.common.domain.model
 class NucleoQuantidadeDeItemEstoque (val id: Int,
                                      val nome: String?,
                                      val sigla: String?,
-                                     val quantidade: Double?)
+                                     val quantidade: Double?){
+
+    var unidadeMedida: UnidadeMedida? = null
+}
