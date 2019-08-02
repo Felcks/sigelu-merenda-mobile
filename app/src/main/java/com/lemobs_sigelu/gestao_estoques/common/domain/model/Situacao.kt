b211fd2d1f56688186b3befed7d4bc7cef95e7f2
Felcks <matheusfelipecorreaalves@gmail.com>
2019-08-02 12:41:37 -0300

    Pequenos ajustes - 11 Cores para lista de pedido melhoria no layout

--- app/src/main/java/com/lemobs_sigelu/gestao_estoques/common/domain/model/Situacao.kt
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/common/domain/model/Situacao.kt
@@ -11,6 +11,7 @@ data class Situacao (
         return when(situacao_id){
             SITUACAO_RASCUNHO -> R.color.pedido_rascunho
             SITUACAO_EM_ANALISE_ID -> R.color.pedido_em_analise
+            SITUACAO_CORRECAO_SOLICITADA -> R.color.pedido_correcao_solicitada
             SITUACAO_APROVADO_ID -> R.color.pedido_aprovado
             SITUACAO_ENTREGUE_ID -> R.color.pedido_entregue
             SITUACAO_REPROVADO_ID -> R.color.pedido_reprovado
@@ -22,11 +23,13 @@ data class Situacao (
     fun getPrioridadeOrdenacao(): Int{
 
         return when(situacao_id){
+            SITUACAO_CORRECAO_SOLICITADA -> 1
+            SITUACAO_RASCUNHO -> 2
+            SITUACAO_APROVADO_ID -> 3
             SITUACAO_EM_ANALISE_ID -> 4
-            SITUACAO_APROVADO_ID -> 2
-            SITUACAO_ENTREGUE_ID -> 5
-            SITUACAO_REPROVADO_ID -> 3
-            SITUACAO_PARCIAL_ID -> 1
+            SITUACAO_PARCIAL_ID -> 5
+            SITUACAO_ENTREGUE_ID -> 6
+            SITUACAO_REPROVADO_ID -> 7
             else -> 10
         }
     }
