4fbbde0e6a75a88c87af02923c6e0b41d6eaa8e7
Felcks <matheusfelipecorreaalves@gmail.com>
2019-09-02 18:28:43 -0300

    Pedido para Núcleo 8 - Layout da tela de confirmação pronta

---
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/cadastra_pedido_3_confirma/ConfirmaCadastroPedidoActivity.kt
@@ -0,0 +1,12 @@
+package com.lemobs_sigelu.gestao_estoques.ui.cadastra_pedido.cadastra_pedido_3_confirma
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmaCadastroPedidoActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_pedido/cadastra_pedido_3_confirma/ConfirmaCadastroPedidoViewModel.kt
@@ -0,0 +1,8 @@
+package com.lemobs_sigelu.gestao_estoques.ui.cadastra_pedido.cadastra_pedido_3_confirma
+
+import androidx.lifecycle.ViewModel
+
+class ConfirmaCadastroPedidoViewModel: ViewModel() {
+
+
+}
