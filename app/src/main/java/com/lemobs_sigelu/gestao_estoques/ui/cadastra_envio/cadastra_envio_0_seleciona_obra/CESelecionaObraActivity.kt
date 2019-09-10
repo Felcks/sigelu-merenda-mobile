f34b2a2ae46e81e8d35e991bea55e69459fa9add
Felcks <matheusfelipecorreaalves@gmail.com>
2019-09-10 14:05:07 -0300

    Envio para obra 3 - Criação das activities e dos layouts

---
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_envio/cadastra_envio_0_seleciona_obra/CESelecionaObraActivity.kt
@@ -0,0 +1,21 @@
+package com.lemobs_sigelu.gestao_estoques.ui.cadastra_envio.cadastra_envio_0_seleciona_obra
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lemobs_sigelu.gestao_estoques.common.domain.model.ActivityDeFluxo
+
+class CESelecionaObraActivity: AppCompatActivity(), ActivityDeFluxo  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun clicouProximo() {
+
+    }
+
+    override fun clicouAnterior() {
+
+    }
+}
---
+++ app/src/main/java/com/lemobs_sigelu/gestao_estoques/ui/cadastra_envio/cadastra_envio_0_seleciona_obra/CESelecionaObraViewModel.kt
@@ -0,0 +1,8 @@
+package com.lemobs_sigelu.gestao_estoques.ui.cadastra_envio.cadastra_envio_0_seleciona_obra
+
+import androidx.lifecycle.ViewModel
+
+class CESelecionaObraViewModel: ViewModel() {
+
+    
+}
